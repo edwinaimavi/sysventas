96ade3d65043ae6474b7d53eb4922f86c96f5200
Edwin <edwin.3acp@gmail.com>
2026-03-30 18:55:21 -0500

    avance sistema

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Spatie\Permission\models\Role;
+use Spatie\Permission\Models\Role;
 use Yajra\DataTables\Facades\DataTables;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\DB;
